4936f76ee0f8b529c650a4e98dd0f75601bc091c
pavel.kv94 <pavel.kv94@yandex.by>
2021-05-24 22:42:31 +0300

    feat: add projects

--- src/header/nav/Nav.js
+++ src/header/nav/Nav.js
@@ -11,3 +11,6 @@ export function Nav() {
         </div>
     )
 }
+
+
+
--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -5,6 +5,8 @@ import { SingleProject } from './singleProject/SingleProject';
 import { Title } from '../common/elements/Title/Title';
 import calcImage from './../assets/images/calc.jpg'
 import meditationImage from './../assets/images/meditation.jpg'
+import momentumImage from './../assets/images/displayApp.jpg'
+import todolistImage from './../assets/images/todolist.jpg'
 
 export function Projects() {
     const calc = {
@@ -12,7 +14,13 @@ export function Projects() {
     };
     const meditation = {
         backgroundImage: `url(${meditationImage})`,
-    }
+    };
+    const momentum = {
+        backgroundImage: `url(${momentumImage})`,
+    };
+    const todolist = {
+        backgroundImage: `url(${todolistImage})`,
+    };
     return (
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`}>
@@ -29,7 +37,18 @@ export function Projects() {
                         description="Lorem asdasddas asd ads adsipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
                         style={meditation}
                         linker="https://pavelkv94.github.io/Meditation-App/"
-
+                    />
+                    <SingleProject
+                        title="Momentum-App"
+                        description="Lorem asdasddas asd ads adsipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                        style={momentum}
+                        linker="https://pavelkv94.github.io/Momentum/"
+                    />
+                    <SingleProject
+                        title="Todolist"
+                        description="Lorem asdasddas asd ads adsipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                        style={todolist}
+                        linker="https://pavelkv94.github.io/Todolist/"
                     />
                 </div>
             </div>
