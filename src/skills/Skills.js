46897a5fe469dd9af75457f2dfa40446bb046f28
pavel.kv94 <pavel.kv94@yandex.by>
2021-05-24 21:42:17 +0300

    feat: add styles for skills Page

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import s from './Skills.module.css'
+import s from './Skills.module.scss'
 import styleContainer from '../common/styles/styleContainer.module.css'
 import { MySkill } from './mySkill/MySkill';
 import { Title } from '../common/elements/Title/Title';
-
+import { faCss3, faGithub, faHtml5, faJs, faReact, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 export function Skills() {
     return (
@@ -11,9 +11,36 @@ export function Skills() {
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <Title title="My skills" />
                 <div className={s.skills}>
-                    <MySkill title="React" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..." />
-                    <MySkill title="HTML" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..." />
-                    <MySkill title="JavaScript" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..." />
+                    <MySkill
+                        icon={faReact}
+                        title="React"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
+                    <MySkill
+                        icon={faHtml5}
+                        title="HTML"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
+                    <MySkill
+                        icon={faJs}
+                        title="JavaScript"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
+                    <MySkill
+                        icon={faCss3}
+                        title="Css3"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
+                    <MySkill
+                        icon={faStackOverflow}
+                        title="StackOverflow"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
+                    <MySkill
+                        icon={faGithub}
+                        title="Git"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
                 </div>
             </div>
         </div>
--- src/skills/mySkill/MySkill.js
+++ src/skills/mySkill/MySkill.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import s from './MySkill.module.css'
+import s from './MySkill.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 
 
 export function MySkill(props) {
     return (
         <div className={s.mySkillBlock}>
-            <div className={s.icon}>s</div>
+            <div className={s.iconBlock}>
+                <FontAwesomeIcon icon={props.icon} className={s.icon}/>
+            </div>
             <h3>{props.title}</h3>
             <span className={s.description}>
                 {props.description}
