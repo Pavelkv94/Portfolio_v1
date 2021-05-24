b1e8386fec8479fd6391004d70e146c250fcc91f
pavel.kv94 <pavel.kv94@yandex.by>
2021-05-24 16:16:32 +0300

    feat: add images for project (via props) & add styles

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -3,20 +3,31 @@ import s from './Projects.module.css'
 import styleContainer from '../common/styles/styleContainer.module.css'
 import { SingleProject } from './singleProject/SingleProject';
 import { Title } from '../common/elements/Title/Title';
-
+import calcImage from './../assets/images/calc.jpg'
+import meditationImage from './../assets/images/meditation.jpg'
 
 export function Projects() {
+    const calc = {
+        backgroundImage: `url(${calcImage})`,
+    };
+    const meditation = {
+        backgroundImage: `url(${meditationImage})`,
+    }
     return (
         <div className={s.projectsBlock}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`}>
-                <Title title = "My projects"/>
+                <Title title="My projects" />
                 <div className={s.projects}>
                     <SingleProject
-                        title="The single greatest gift to get your friend who always travels"
-                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..." />
+                        title="Calculator"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                        style={calc}
+                    />
                     <SingleProject
-                        title="The single greatest gift to get your friend who always travels"
-                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..." />
+                        title="Meditation-App"
+                        description="Lorem asdasddas asd ads adsipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                        style={meditation}
+                    />
                 </div>
             </div>
         </div>
--- src/projects/singleProject/SingleProject.js
+++ src/projects/singleProject/SingleProject.js
@@ -6,13 +6,15 @@ import s from './SingleProject.module.css'
 export function SingleProject(props) {
     return (
         <div className={s.project}>
-           <div className={s.image}>
-             <Button title="view"/>
-           </div>
-           <h3 className={s.title}>{props.title}</h3>
-           <span className={s.description}>
-                {props.description}
-            </span>
+            <div className={s.image} style={props.style}>
+                <Button title="Look" />
+            </div>
+            <div className={s.projectInfo}>
+                <h3 className={s.title}>{props.title}</h3>
+                <span className={s.description}>
+                    {props.description}
+                </span>
+            </div>
         </div>
     )
 }
