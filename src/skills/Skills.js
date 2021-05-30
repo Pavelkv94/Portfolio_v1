2c4595bd9ed9d456b8a1c8c2d37976f4a2637c78
pavel.kv94 <pavel.kv94@yandex.by>
2021-05-30 22:27:30 +0300

    fix: fix font

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -41,6 +41,11 @@ export function Skills() {
                         title="Git"
                         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
                     />
+                     <MySkill
+                        icon={faGithub}
+                        title="Git"
+                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim..."
+                    />
                 </div>
             </div>
         </div>
