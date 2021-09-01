fa9a6e53b54c1ac720ae10bbe3d4ddea57f58d6d
Pavelkv94 <pavel.kv94@yandex.by>
2021-09-01 13:15:17 +0300

    feat: form for feedback

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -4,19 +4,29 @@ import styleContainer from '../common/styles/styleContainer.module.css'
 import { Button } from '../common/elements/button/Button';
 import { Title } from '../common/elements/Title/Title';
 import Fade from 'react-reveal/Fade';
+import axios from 'axios';
 
 export function Contacts() {
+    const submitCallback = (e) => {
+        e.preventDefault();
+        //console.log(e.target[0].value)
+        axios.post("http://localhost:3010/send", { name: e.target[0].value, email: e.target[1].value, message: e.target[2].value })
+            .then(() => { alert('Your message has been sent') })
+    }
+
     return (
-        <div className={s.contactsBlock} id="contacts">
+        <div className={s.contactsBlock} id="contacts" >
             <Fade bottom>
                 <div className={`${styleContainer.container} ${s.contactsContainer}`}>
                     <Title title="Contacts" />
-                    <form className={s.contacts}>
-                        <input type="text" placeholder="Name" className={s.input} />
-                        <input type="email" placeholder="email" className={s.input} />
-                        <textarea placeholder="Your messaage" className={s.textarea} />
+                    <form className={s.contacts} onSubmit={submitCallback}>
+                        <input type="text" placeholder="Name" name="name" className={s.input} />
+                        <input type="email" placeholder="email" name="email" className={s.input} />
+                        <textarea placeholder="Your messaage" name="message" className={s.textarea} />
+                        <button >Send</button>
+                        {/* <Button title="Send" type={"submit"} linker="#" onSubmit={(e) => { alert('Submit') }} /> */}
                     </form>
-                    <Button title="Send" type={"submit"} linker="#" />
+
                 </div>
             </Fade>
         </div>
